58ebc4164b2a7852445804e69bed8d8e914632d0
C1dy <sashaculle@gmail.com>
2024-03-30 22:50:58 +0100

    3 task

--- 1 task.py
+++ 1 task.py
@@ -1,12 +1,14 @@
-class City:
-    def __init__(self,city_name,region,country,amount_of_people_living,post_index,telephone_code):
-        self.city_name = city_name
-        self.region = region
-        self.country = country
-        self.amount_of_people_living = amount_of_people_living
-        self.post_index = post_index
+class Country:
+    def __init__(self,name_of_country,continent,people_living,telephone_code,main_city,list_of_cities):
+        self.name_of_country = name_of_country
+        self.continent = continent
+        self.people_living = people_living
         self.telephone_code = telephone_code
+        self.main_city = main_city
+        self.list_of_cities = list_of_cities
+    def add_city(self,new_city):
+        self.list_of_cities.append(new_city)
+    def change_capitol(self,new_capitol):
+        self.main_city = new_capitol
     def get_info(self):
-        return self.city_name,self.region,self.post_index,self.amount_of_people_living,self.telephone_code
-    def example_change_method(self,new_value):
-        self.city_name = new_value
+        return self.name_of_country,self.continent,self.people_living,self.telephone_code,self.main_city,self.list_of_cities

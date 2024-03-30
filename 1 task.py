d99e4bb9a3c6e81de7fed473d8e7bfeaf45c3e46
C1dy <sashaculle@gmail.com>
2024-03-30 22:46:24 +0100

    2 task

--- 1 task.py
+++ 1 task.py
@@ -1,12 +1,12 @@
-class Human:
-    def __init__(self,PiB,date_of_birth,phone_number,city,country,adress):
-        self._PiB = PiB
-        self._date_of_birth = date_of_birth
-        self._phone_number = phone_number
-        self._city = city
-        self._country = country
-        self._adress = adress
+class City:
+    def __init__(self,city_name,region,country,amount_of_people_living,post_index,telephone_code):
+        self.city_name = city_name
+        self.region = region
+        self.country = country
+        self.amount_of_people_living = amount_of_people_living
+        self.post_index = post_index
+        self.telephone_code = telephone_code
     def get_info(self):
-        return self._PiB,self._city,self._adress,self._date_of_birth,self._phone_number
+        return self.city_name,self.region,self.post_index,self.amount_of_people_living,self.telephone_code
     def example_change_method(self,new_value):
-        self._PiB = new_value
+        self.city_name = new_value
